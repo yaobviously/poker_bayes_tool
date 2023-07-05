4c4419ec604baf123c06318acc3c3fd9b4792ff7
yaobviously <yaobviously@gmail.com>
2023-07-05 18:26:23 -0400

    adding the function and tool

---
+++ app.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 14:23:42 2023
+
+@author: yaobv
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+
+from scipy.stats import beta, binom
+from update_func import update_poker_beliefs
+
+
+def main():
+
+    st.title("Poker Belief Updater")
+
+    # Create a sidebar column for inputs
+    with st.sidebar:
+        st.subheader("Input Parameters")
+        hands = st.slider('Sample of Hands', min_value=1, max_value=100, value=15, help="Number of hands")
+        raises = st.slider('Aggressive Actions', min_value=1, max_value=hands, value=4, help="Number of aggressive actions")
+        value_perc = st.slider('Value Hands', min_value=0.01, max_value=0.3, step=0.01, value=0.15, help="Percentage of value hands")
+        prior = st.selectbox('Prior', ['beta_1', 'beta_2', 'beta_3', 'uniform'], help="Prior distribution")
+
+    # Call the 'update_poker_beliefs' function and get the plot
+    plot = update_poker_beliefs(hands, raises, value_perc, prior)
+
+    # Display the plot using Streamlit
+    st.pyplot(plot)
+
+
+if __name__ == '__main__':
+    main()
---
+++ update_func.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  5 14:30:28 2023
+
+@author: yaobv
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from scipy.stats import beta, binom
+
+def update_poker_beliefs(hands: int, raises: int, value_perc: float, prior: str) -> None:
+    
+    possible_bluff_freq = np.linspace(0.00, 1.0, 101)
+    
+    prior_dict = {
+        'beta_1' : beta.pdf(possible_bluff_freq, 1, 3),
+        'beta_2' : beta.pdf(possible_bluff_freq, 2, 5),
+        'beta_3' : beta.pdf(possible_bluff_freq, 4, 10),
+        'uniform' : np.full(len(possible_bluff_freq), 1)
+    }
+    prior = prior_dict[prior]
+    prior = prior / prior.sum()
+    
+    val_bet_probs = []
+    
+    bluff_df = pd.DataFrame()
+    
+    for val in np.arange(0, raises + 1):
+        
+        prob_val_ = binom.pmf(val, hands, value_perc)
+        prob_bluffs_ = pd.Series(binom.pmf(raises-val, hands-val, possible_bluff_freq))
+        prob_bluffs_.name = f'{raises - val}_bluffs'        
+        
+        val_bet_probs.append(prob_val_)
+        bluff_df = pd.concat([bluff_df, prob_bluffs_], axis=1)
+        
+    # normalizing the value bet probabilities
+    # these are the probabilities of value bets given value bet %
+    # and they're used to weight the bluffing frequencies
+    val_bet_probs = np.array(val_bet_probs)
+    val_bet_probs = val_bet_probs / val_bet_probs.sum()
+    
+    # multiplying each bluff count by its probability of occurring
+    bluff_df = bluff_df * val_bet_probs
+    
+    # getting the total weighted likelihood for each bluff frequency
+    bluff_df['total_prob'] = bluff_df.sum(axis=1)
+    
+    # getting the unnormed posterior and normalizing it
+    unnormed = bluff_df['total_prob'] * prior
+    posterior = unnormed / unnormed.sum()
+    
+    # Create the plot object
+    fig, ax = plt.subplots(figsize=(5, 3))
+    plt.title(f"Update After {raises} xr in {hands} hands")
+    plt.plot(possible_bluff_freq, posterior, label="posterior")
+    plt.plot(possible_bluff_freq, prior, label="prior")
+    plt.xlabel("Opponent's Bluffing Frequency")
+    plt.legend()
+
+    # Return the plot object
+    return fig
